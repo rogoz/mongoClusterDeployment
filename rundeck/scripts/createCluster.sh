912e002277f45ef2466a3563475082bd58db6983
Tudor Rogoz <rogoz@adobe.com>
2013-07-22 10:13:06 +0300

    Add create&destroy cluster scripts

--- rundeck/scripts/createCluster.sh
+++ rundeck/scripts/createCluster.sh
@@ -5,25 +5,32 @@ PATH=${PATH}:${RUNDECK_HOME}tools/bin
 HDD_SIZE=25
 PROVISIONR_PATH=/home/tudor/repos/incubator-provisionr/karaf/assembly/target/org.apache.provisionr-0.4.0-incubating-SNAPSHOT/bin/
 
+while getopts s:m: option
+do
+        case "${option}"
+        in
+                s) SHARDS_NUMBER=${OPTARG};;
+                m) MONGOS_NUMBER=${OPTARG};;
+        esac
+done
 
 # Create shards
 echo "***** Step 1: Creating cluster's shards. *****"
-echo "Number of shards ${SHARDS_NUMBER}.ETA 10 minutes."
-run -i 7 --follow -- -SHARDS_NUMBER ${SHARDS_NUMBER} -HDD_SIZE ${HDD_SIZE} -PROVISIONR_PATH ${PROVISIONR_PATH}
+run -i 11 --follow -- -SHARDS_NUMBER ${SHARDS_NUMBER} -HDD_SIZE ${HDD_SIZE} -PROVISIONR_PATH ${PROVISIONR_PATH}
 echo "***** Step 1: Completed. *****"
 
 # Configure shards
 echo "***** Step 2: Configure cluster's shards. *****"
-run -i 1 --follow
+run -i 12 --follow
 echo "***** Step 2: Completed. *****"
 
 # Create mongos
 echo "***** Step 3: Create mongos platforms. *****"
-run -i 3 --follow -- -MONGOS_NUMBER ${MONGOS_NUMBER} -PROVISIONR_PATH ${PROVISIONR_PATH}
+run -i 13 --follow -- -MONGOS_NUMBER ${MONGOS_NUMBER} -PROVISIONR_PATH ${PROVISIONR_PATH}
 echo "***** Step 3: Completed. *****"
 
 #Configure mongos
 echo "***** Step 4: Configure mongos platforms. *****"
-run -i 6 --follow
+run -i 14 --follow
 echo "***** Step 4: Completed. *****"
 
--- rundeck/scripts/destroyCluster.sh
+++ rundeck/scripts/destroyCluster.sh
@@ -1,11 +1,22 @@
 #!/bin/bash 
 
-# Step1
-# Create database cluster node
+# Destroy mongo cluster
 
-RAND=$(( $RANDOM % 1000 ))
-# KEY=k${RAND}
+RUNDECK_HOME=/home/tudor/Tools/rundeck/
 PROVISIONR_PATH=/home/tudor/repos/incubator-provisionr/karaf/assembly/target/org.apache.provisionr-0.4.0-incubating-SNAPSHOT/bin/
-KEY=$1
-${PROVISIONR_PATH}client "provisionr:destroy -k ${KEY}"
-# wait for the instances to be created
+
+MONGOS_KEY=`xmllint --xpath 'string(/project/node[1]/attribute[2]/@value)' ${RUNDECK_HOME}mongos.xml`
+SHARDS_KEY=`xmllint --xpath 'string(/project/node[1]/attribute[2]/@value)' ${RUNDECK_HOME}shards.xml` 
+echo "MONGOS KEY = "${MONGOS_KEY} 
+echo "SHARDS KEY = "${SHARDS_KEY}
+
+${PROVISIONR_PATH}client "provisionr:destroy -k ${MONGOS_KEY}"
+sleep 10
+${PROVISIONR_PATH}client "provisionr:destroy -k ${SHARDS_KEY}"
+sleep 10
+
+
+
+
+ 
+
