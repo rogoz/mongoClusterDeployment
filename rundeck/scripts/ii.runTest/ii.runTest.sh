57f734d0a7c7110e78a24493c739b1ef87d89d7c
Tudor Rogoz <rogoz@adobe.com>
2013-10-03 14:49:16 +0300

    Add the OAKS_NUMBER parameter

--- rundeck/scripts/ii.runTest/ii.runTest.sh
+++ rundeck/scripts/ii.runTest/ii.runTest.sh
@@ -3,7 +3,18 @@
 TEST_NAME=$1
 CURRENT_NODE=$2
 DROP_DB=$3
+OAKS_NUMBER=$4
 DATABASE_NAME=Oak-database-$RANDOM
+
+TEMP=`xmllint --xpath '/project/node/@hostname' shards.xml|sed -e "s/ hostname=/ /g"| sed -e "s/\"/'/g"` 
+declare -a all_mongos=($TEMP)
+for i in {1..$OAKS_NUMBER}
+do
+ mongos_trim=`echo ${all_mongos[i-1]}|tr -d ''\'''`
+ mongos[i-1]=${mongos_trim}
+done
+echo "test_platoforms=${mongos[@]}"
+
 MONGOS_PORT=27017
 MONGOS_MAIN_PLATFORM=`xmllint --xpath 'string(/project/node[1]/@hostname)' mongos.xml`
 TIMEOUT=60
@@ -26,8 +37,8 @@ if [ "$CURRENT_NODE" == "$MONGOS_MAIN_PLATFORM" ]; then
  java -jar /home/${USER}/jackrabbit-oak/oak-run/target/oak-run-*.jar benchmark $TEST_NAME Oak-Mongo --db $DATABASE_NAME --dropDBAfterTest $DROP_DB
 else
  sleep $TIMEOUT
- echo "Start tests on the secondary mongos platform"
- java -jar /home/${USER}/jackrabbit-oak/oak-run/target/oak-run-*.jar benchmark $TEST_NAME Oak-Mongo --db $DATABASE_NAME --dropDBAfterTest $DROP_DB
+ # Starts the tests only if is part of the 
+ echo ${mongos[@]}}|grep ${CURRENT_NODE}&&java -jar /home/${USER}/jackrabbit-oak/oak-run/target/oak-run-*.jar benchmark $TEST_NAME Oak-Mongo --db $DATABASE_NAME --dropDBAfterTest $DROP_DB
 fi
 
 
