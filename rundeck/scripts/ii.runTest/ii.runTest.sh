bf810b19d1852740536906e1b00493b0630d3f5b
Tudor Rogoz <rogoz@adobe.com>
2013-10-03 11:42:08 +0300

    Add test script

--- rundeck/scripts/ii.runTest/ii.runTest.sh
+++ rundeck/scripts/ii.runTest/ii.runTest.sh
@@ -2,9 +2,12 @@
 
 TEST_NAME=$1
 CURRENT_NODE=$2
+DROP_DB=$3
 DATABASE_NAME=Oak-database-$RANDOM
 MONGOS_PORT=27017
 MONGOS_MAIN_PLATFORM=`xmllint --xpath 'string(/project/node[1]/@hostname)' mongos.xml`
+TIMEOUT=60
+
 # Download and set Java  
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk6-downloads-1637591.html;" http://download.oracle.com/otn-pub/java/jdk/6u33-b03/jdk-6u33-linux-x64.bin   
 chmod a+x jdk-6u33-linux-x64.bin
@@ -20,10 +23,11 @@ if [ "$CURRENT_NODE" == "$MONGOS_MAIN_PLATFORM" ]; then
  mongo --host localhost $DATABASE_NAME --port $MONGOS_PORT --eval "sh.shardCollection(\"$DATABASE_NAME.nodes\", { \"_id\": 1 }, true)" 
  mongo --host localhost $DATABASE_NAME --port $MONGOS_PORT --eval "sh.shardCollection(\"$DATABASE_NAME.blobs\", { \"_id\": 1 }, true)" 
  echo "Start tests on the main mongos platform"
- java -jar /home/${USER}/jackrabbit-oak/oak-run/target/oak-run-*.jar benchmark $TEST_NAME Oak-Mongo
+ java -jar /home/${USER}/jackrabbit-oak/oak-run/target/oak-run-*.jar benchmark $TEST_NAME Oak-Mongo --db $DATABASE_NAME --dropDBAfterTest $DROP_DB
 else
- sleep 60
- java -jar /home/${USER}/jackrabbit-oak/oak-run/target/oak-run-*.jar benchmark $TEST_NAME Oak-Mongo
+ sleep $TIMEOUT
+ echo "Start tests on the secondary mongos platform"
+ java -jar /home/${USER}/jackrabbit-oak/oak-run/target/oak-run-*.jar benchmark $TEST_NAME Oak-Mongo --db $DATABASE_NAME --dropDBAfterTest $DROP_DB
 fi
 
 
