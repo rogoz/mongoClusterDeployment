7d6c9b8225d5dc4a92630fb0feb496bda1c79d7a
Tudor Rogoz <rogoz@adobe.com>
2013-10-03 10:35:29 +0300

    Fix minor issue

--- rundeck/scripts/i.getTests/i.getTests.sh
+++ rundeck/scripts/i.getTests/i.getTests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo Clone oak repository  
+rm -rf jackrabbit-oak
 git clone https://github.com/apache/jackrabbit-oak.git  
 # build oak
 mvn -f ./jackrabbit-oak/pom.xml clean install -DskipTests 
