038a63fe95b6b68ea5e38c7072ddf3f9ef5c87f2
Tudor Rogoz <rogoz@adobe.com>
2013-10-03 18:02:57 +0300

    Minor fix

--- rundeck/scripts/ii.runTest/ii.runTest.sh
+++ rundeck/scripts/ii.runTest/ii.runTest.sh
@@ -13,12 +13,10 @@ do
  mongos_trim=`echo ${all_mongos[$i]}|tr -d ''\'''`
  mongos[$i]=${mongos_trim}
 done
-echo "test_platoforms=${mongos[@]}"
-
+echo "test_platforms=${mongos[@]}"
 # Download and set Java  
 export JAVA_HOME=/home/${USER}/jdk1.6.0_33/
 export PATH=${JAVA_HOME}bin:$PATH
-
 echo ${mongos[@]}}|grep ${CURRENT_NODE}&&java -jar /home/${USER}/jackrabbit-oak/oak-run/target/oak-run-*.jar benchmark $TEST_NAME Oak-Mongo --db $DATABASE_NAME --dropDBAfterTest $DROP_DB
 
 
