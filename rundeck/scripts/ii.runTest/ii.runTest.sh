7530ae93d68b1cdaea17ce8e9bc4f06ad8b0c5f9
Tudor Rogoz <rogoz@adobe.com>
2013-10-04 13:01:01 +0300

    Improve logging

--- rundeck/scripts/ii.runTest/ii.runTest.sh
+++ rundeck/scripts/ii.runTest/ii.runTest.sh
@@ -17,10 +17,11 @@ echo "test_platforms=${mongos[@]}"
 # Download and set Java  
 export JAVA_HOME=/home/${USER}/jdk1.6.0_33/
 export PATH=${JAVA_HOME}bin:$PATH
-echo $CURRENT_NODE
+echo CURRENT_NODE=$CURRENT_NODE
 echo ${mongos[@]}|grep ${CURRENT_NODE}
 STATUS=$?
 if [ "$STATUS" -eq "0" ]; then 
+ echo "TEST executed on mongos->$CURRENT_NODE"
  java -jar /home/${USER}/jackrabbit-oak/oak-run/target/oak-run-*.jar benchmark $TEST_NAME Oak-Mongo --db $DATABASE_NAME --dropDBAfterTest $DROP_DB
 fi
 
