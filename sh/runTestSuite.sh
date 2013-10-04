335de259c480f751e61d356c97a09fe968d9f313
Tudor Rogoz <rogoz@adobe.com>
2013-10-04 10:34:52 +0300

    Add run testsuite script

---
+++ sh/runTestSuite.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#timeout between tests
+TIMEOUT=40
+ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+RUNDECK_HOME=${ABSOLUTE_PATH}/rundeck/
+PATH=${PATH}:${RUNDECK_HOME}tools/bin
+DROP_DB=false
+INIT_TEST=LoginTest
+tests=( "$@" )
+OAKS_NUMBER=1
+
+echo "The following tests will be executed ${tests[@]}"
+
+for test in "${tests[@]}"
+do
+ # init database
+ DATABASE_SUFIX=$RANDOM
+ echo "Running ${test} on database Oak-database-$DATABASE_SUFIX"
+ run -j init.repository --follow -- -TEST_NAME ${INIT_TEST} -DATABASE_NAME Oak-database-$DATABASE_SUFIX
+ run -j ii-runTest --follow -- -TEST_NAME ${test} -DROP_DB ${DROP_DB} -OAKS_NUMBER $OAKS_NUMBER -DATABASE_NAME Oak-database-$DATABASE_SUFIX
+ sleep $TIMEOUT
+done 
+
