bf1e8dea8ffc602ece965f83143d232c9187ce2a
Tudor Rogoz <rogoz@adobe.com>
2013-10-08 11:27:51 +0300

    Test monitoring

--- rundeck/scripts/5.connectToMMS/connectToMMS.sh
+++ rundeck/scripts/5.connectToMMS/connectToMMS.sh
@@ -6,8 +6,7 @@ MMS_PASS=$2
 MONGOS_HOST=`xmllint --xpath 'string(/project/node[1]/@hostname)' mongos.xml`
 MONGOS_PORT=27017
 
-echo User=$MMS_USER
-echo Pass=$MMS_PASS
+
 
 # login
 curl -c cookies.txt 'https://mms.mongodb.com/user/v1/auth' -H 'Origin: https://mms.mongodb.com' -H 'Accept-Encoding: gzip,deflate,sdch' -H 'Host: mms.mongodb.com' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36' -H 'Content-Type: application/json; charset=UTF-8' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://mms.mongodb.com/' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --data-binary "{\"username\":\"$MMS_USER\",\"password\":\"$MMS_PASS\"}" --compressed
--- sh/monitorCluster.sh
+++ sh/monitorCluster.sh
@@ -7,8 +7,6 @@ PATH=${PATH}:${RUNDECK_HOME}tools/bin
 MMS_USER=$1
 MMS_PASS=$2
 
-echo User=$MMS_USER
-echo Pass=$MMS_PASS
 echo "***** Add cluster to the Mongo Management Service"
 run -j 5-monitorCluster --follow -- -MMS_USER ${MMS_USER} -MMS_PASS ${MMS_PASS}
 
