462460ae1b020b7b3f9f75ddb3043574d4dc9c4d
Tudor Rogoz <rogoz@adobe.com>
2013-10-08 10:15:32 +0300

    Add monitor cluster generic script

---
+++ sh/monitorCluster.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+RUNDECK_HOME=${ABSOLUTE_PATH}/rundeck/
+PATH=${PATH}:${RUNDECK_HOME}tools/bin
+
+MMS_USER=$1
+MMS_PASS=$2
+
+echo "***** Add cluster to the Mongo Management Service"
+run -j 5-monitorCluster --follow -- -MMS_USER ${MMS_USER} -MMS_PASS ${MMS_PASS}
+
+
+
