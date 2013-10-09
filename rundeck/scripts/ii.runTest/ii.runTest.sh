f198d6312075a38ed9ced6237701805911df56c1
Tudor Rogoz <rogoz@adobe.com>
2013-10-09 12:47:30 +0300

    Write the test results into a file

--- rundeck/scripts/ii.runTest/ii.runTest.sh
+++ rundeck/scripts/ii.runTest/ii.runTest.sh
@@ -7,6 +7,12 @@ OAKS_NUMBER=$4
 DATABASE_NAME=$5
 MONGOS_PORT=27017
 TEMP=`xmllint --xpath '/project/node/@hostname' mongos.xml|sed -e "s/ hostname=/ /g"| sed -e "s/\"/'/g"` 
+
+
+#cleanup first 
+rm -rf ~/results/
+mkdir ~/results/
+
 declare -a all_mongos=($TEMP)
 for (( i=0; i<${OAKS_NUMBER}; i++ ))
 do
@@ -23,7 +29,7 @@ STATUS=$?
 if [ "$STATUS" -eq "0" ]; then 
  echo "TEST executed on mongos->$CURRENT_NODE"
  echo Running command="java -Dwarmup=0 -Druntime=10 -jar /home/${USER}/jackrabbit-oak/oak-run/target/oak-run-*.jar benchmark $TEST_NAME Oak-Mongo --db $DATABASE_NAME --dropDBAfterTest $DROP_DB"
- java -Dwarmup=0 -Druntime=10 -jar /home/${USER}/jackrabbit-oak/oak-run/target/oak-run-*.jar benchmark $TEST_NAME Oak-Mongo --db $DATABASE_NAME --dropDBAfterTest $DROP_DB
+ java -Dwarmup=0 -Druntime=5 -jar /home/${USER}/jackrabbit-oak/oak-run/target/oak-run-*.jar benchmark $TEST_NAME Oak-Mongo --db $DATABASE_NAME --dropDBAfterTest $DROP_DB|tee ~/results/${TEST_NAME} 
 fi
 
 
