50c01d29c980b2adce18542a66ec8b98132604d1
Tudor Rogoz <rogoz@adobe.com>
2013-10-13 14:51:29 +0300

    move getTests to oakScalability

--- sh/getTests.sh
+++ sh/getTests.sh
@@ -4,4 +4,4 @@ ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 RUNDECK_HOME=${ABSOLUTE_PATH}/rundeck/
 PATH=${PATH}:${RUNDECK_HOME}tools/bin
 echo $PATH
-run -j i-getTests --follow
+run -j i-getTests -p oakScalability --follow
