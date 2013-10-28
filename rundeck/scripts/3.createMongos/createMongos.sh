f79cd29c82f5c601b1d96cf73abcf29784d39cb7
rogoz@adobe.com <rogoz@adobe.com>
2013-10-28 15:47:00 +0200

    Add jmeter tool

--- rundeck/scripts/3.createMongos/createMongos.sh
+++ rundeck/scripts/3.createMongos/createMongos.sh
@@ -17,7 +17,7 @@ MONITORING_AGENT_KEY=s27947
 # creates new instances on amazon
 ${PROVISIONR_PATH}client "provisionr:create --id amazon --key ${MONGOS_KEY} --size ${MONGOS_NUMBER} --hardware-type m1.large --template mongos --image-id ami-4965f479 --timeout 600"
 # wait for the instances to be created
-sleep 700
+sleep 900
 
 # create mongos connection file on localhost
 rm -rf mongos.xml 
