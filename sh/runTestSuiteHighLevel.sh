31fafb50439b16340a7427b4f69ed44ac33f98bd
rogoz@adobe.com <rogoz@adobe.com>
2013-10-30 10:50:09 +0200

    Add runHighLevel rundeck jobs

---
+++ sh/runTestSuiteHighLevel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TIMEOUT=40
+ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+RUNDECK_HOME=${ABSOLUTE_PATH}/rundeck/
+PATH=${PATH}:${RUNDECK_HOME}tools/bin
+
+tests=( "$@" )
+OAKS_NUMBER=${ENV_OAKS_NUMBER}
+echo "The following tests will be executed ${tests[@]}"
+
+for test in "${tests[@]}"
+do
+ echo "Running ${test}"
+ run -j d-runTests -p oakScalabilityHighLevel --follow -- -TEST_NAME ${TEST_NAME} -RUNTIME ${RUNTIME} -OAKS_NUMBER $OAKS_NUMBER
+ sleep $TIMEOUT
+done 
