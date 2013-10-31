b0a373fa8ba0c43035693495a2292b669a890188
rogoz@adobe.com <rogoz@adobe.com>
2013-10-31 15:53:04 +0200

    Fix minor

--- rundeck/scripts/6.installJava/installJava.sh
+++ rundeck/scripts/6.installJava/installJava.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 JMETER_HOME=/usr/share/jmeter/
-JMETER_EX_HOME=${JMETER_HOMElib}/ext
+JMETER_EX_HOME=${JMETER_HOME}lib/ext
 # Download and set Java
 if [ ! -f jdk-6u33-linux-x64.bin ]; then
     wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk6-downloads-1637591.html;" http://download.oracle.com/otn-pub/java/jdk/6u33-b03/jdk-6u33-linux-x64.bin   
