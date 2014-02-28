6333afdfc4b6db53ac18e6b2f4d5abac546aa8c5
rogoz@adobe.com <rogoz@adobe.com>
2014-02-28 12:34:43 +0100

    Add extra parameter for the jar

--- sh/getProduct.sh
+++ sh/getProduct.sh
@@ -2,8 +2,9 @@
 
 ID=$1
 KEY=$2
+PRODUCT_NAME=$3
 ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 RUNDECK_HOME=${ABSOLUTE_PATH}/rundeck/
 PATH=${PATH}:${RUNDECK_HOME}tools/bin
 
-run -j a-downloadProduct -p oakScalabilityHighLevel --follow -- -ID ${ID} -KEY ${KEY}
+run -j a-downloadProduct -p oakScalabilityHighLevel --follow -- -ID ${ID} -KEY ${KEY} -PRODUCT_NAME ${PRODUCT_NAME}
