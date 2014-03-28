8024d8a2abbb0e3b5222f35eb669aeaef13597c2
rogoz@adobe.com <rogoz@adobe.com>
2014-03-28 11:29:55 +0200

    Minor fix

--- rundeck/scripts/4.configureMongos/configureMongos2.sh
+++ rundeck/scripts/4.configureMongos/configureMongos2.sh
@@ -36,7 +36,7 @@ TEST_COMMAND='mongo --eval "printjson(db.serverStatus())"'
 retry "${TEST_COMMAND}" 
 
 echo "jenkins soft nofile 500000" | sudo tee /etc/security/limits.conf
-echo "jenkins hard nofile 500000" | sudo -a tee /etc/security/limits.conf
+echo "jenkins hard nofile 500000" | sudo tee -a /etc/security/limits.conf
 
 
 
